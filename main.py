6b81f1b602ebbb0c90a8dc74ed1160bb52689d4c
Sta4ik <ctac354.354@gmail.com>
2026-06-16 21:37:39 +0300

    add exception handler in bot command

--- main.py
+++ main.py
@@ -16,78 +16,93 @@ async def ping(ctx):
 
 @bot.command()
 async def last_match(ctx, deadlock_id):
-    stats = await api.get_player_last_match(int(deadlock_id))
+    try:
+        stats = await api.get_player_last_match(int(deadlock_id))
 
-    if not stats:
+        if not stats:
+            await ctx.send(f'Ошибка получения последнего матча игрока {deadlock_id}')
+            return
+
+        hero = await api.get_hero_by_id(stats["hero_id"])
+
+        if not hero:
+            await ctx.send(f'Ошибка получения героя с id {stats["hero_id"]}')
+            return
+        
+        hero_name = hero["name"]
+        hero_icon = hero["images"]["icon_image_small"]
+
+        color = (discord.Color.red() if stats["match_result"] == 0 else discord.Color.green())
+
+        result = "Поражение" if stats["match_result"] == 0 else "Победа"
+
+        duration_min = stats["match_duration_s"] // 60
+        duration_sec = stats["match_duration_s"] % 60
+
+        embed = discord.Embed(
+            title=f"Последний матч игрока {deadlock_id}",
+            description=f"**{hero_name}**",
+            color=color
+        )
+
+        embed.set_thumbnail(url=hero_icon)
+
+        embed.add_field(name="Матч ID", value=stats["match_id"], inline=False)
+        embed.add_field(name="K/D/A", value=f"{stats['player_kills']}/{stats['player_deaths']}/{stats['player_assists']}", inline=True)
+        embed.add_field(name="Нетворс", value=stats["net_worth"], inline=True)
+        embed.add_field(name="Длительность", value=f"{duration_min}:{duration_sec} мин", inline=True)
+
+        embed.add_field(name="Команда", value=("The Hidden King" if stats["player_team"] == 0 else "The ArchMother"), inline=True)
+        embed.add_field(name="Результат", value=result, inline=True)
+
+        embed.set_footer(text="Deadlock stater")
+
+        await ctx.send(embed=embed)
+
+    except Exception as e:
+        print("Error:", e)
         await ctx.send(f'Ошибка получения последнего матча игрока {deadlock_id}')
-        return
-
-    hero = await api.get_hero_by_id(stats["hero_id"])
-
-    if not hero:
-        await ctx.send(f'Ошибка получения героя с id {stats["hero_id"]}')
-        return
-    
-    hero_name = hero["name"]
-    hero_icon = hero["images"]["icon_image_small"]
-
-    color = (discord.Color.red() if stats["match_result"] == 0 else discord.Color.green())
-
-    result = "Поражение" if stats["match_result"] == 0 else "Победа"
-
-    duration_min = stats["match_duration_s"] // 60
-    duration_sec = stats["match_duration_s"] % 60
-
-    embed = discord.Embed(
-        title=f"Последний матч игрока {deadlock_id}",
-        description=f"**{hero_name}**",
-        color=color
-    )
-
-    embed.set_thumbnail(url=hero_icon)
-
-    embed.add_field(name="Матч ID", value=stats["match_id"], inline=False)
-    embed.add_field(name="K/D/A", value=f"{stats['player_kills']}/{stats['player_deaths']}/{stats['player_assists']}", inline=True)
-    embed.add_field(name="Нетворс", value=stats["net_worth"], inline=True)
-    embed.add_field(name="Длительность", value=f"{duration_min}:{duration_sec} мин", inline=True)
-
-    embed.add_field(name="Команда", value=("The Hidden King" if stats["player_team"] == 0 else "The ArchMother"), inline=True)
-    embed.add_field(name="Результат", value=result, inline=True)
-
-    embed.set_footer(text="Deadlock stater")
-
-    await ctx.send(embed=embed)
 
 @bot.command()
 async def get_id(ctx, steam_id):
-    id = await api.get_id_by_steam_id(int(steam_id))
-    
-    if not id:
+    try:
+        id = await api.get_id_by_steam_id(int(steam_id))
+        
+        if not id:
+            await ctx.send(f"Ошибка получения deadlock id с Steam id {steam_id}")
+            return
+        
+        await ctx.send(id)
+
+    except Exception as e:
+        print("Error:", e)
         await ctx.send(f"Ошибка получения deadlock id с Steam id {steam_id}")
-        return
-    
-    await ctx.send(id)
 
 @bot.command()
 async def whois(ctx, account_id):
-    steam_info = await api.get_steam_profile_by_id(int(account_id))
+    try:
+        steam_info = await api.get_steam_profile_by_id(int(account_id))
 
-    if not steam_info:
+        if not steam_info:
+            await ctx.send(f"Ошибка получения стим профиля с deadlock id {account_id}")
+            return
+
+        embed = discord.Embed(
+            title=steam_info["name"],
+            url=steam_info["steam_url"],
+            color=discord.Color.blue()
+        )
+
+        embed.set_thumbnail(url=steam_info["avatar"])
+
+        embed.add_field(name="Настоящее имя", value=steam_info["realname"] or "Не указано", inline=False)
+        embed.add_field(name="Матчей за 30 дней", value=steam_info["matches_played_last_30d"], inline=True)
+
+        await ctx.send(embed=embed)
+
+    except Exception as e:
+        print("Error:", e)
         await ctx.send(f"Ошибка получения стим профиля с deadlock id {account_id}")
-        return
-
-    embed = discord.Embed(
-        title=steam_info["name"],
-        url=steam_info["steam_url"],
-        color=discord.Color.blue()
-    )
-
-    embed.set_thumbnail(url=steam_info["avatar"])
-
-    embed.add_field(name="Настоящее имя", value=steam_info["realname"] or "Не указано", inline=False)
-    embed.add_field(name="Матчей за 30 дней", value=steam_info["matches_played_last_30d"], inline=True)
-
-    await ctx.send(embed=embed)
 
 if __name__ == "__main__":
     bot.run(cr.TOKEN)
